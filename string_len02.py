d158f99a86e609223cb46768793e337bb20eaab3
M A C W I N <mavzurovnurali@gmail.com>
2025-05-02 21:29:42 +0500

    SSS

--- string_len02.py
+++ string_len02.py
@@ -1,9 +1,19 @@
 def main(a):
     """
     A string type variable is given. Return True if its length is even. Return False if its length is odd.
+
     Args:
-        a: string
+        a (str): string
+
     Returns:
-        True or False
+        bool: True or False
     """
-    return
+    if len(a) % 2 == 0:
+        return True
+    else:
+        return False
+
+# Sinov
+a = "fjfhfhdhgshgs"
+print(main(a))  # False, chunki uzunligi 13 (toq)
+
