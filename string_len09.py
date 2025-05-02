3d1f4bb3bda5b18f003c2614f975b636cee05a5d
M A C W I N <mavzurovnurali@gmail.com>
2025-05-02 22:40:36 +0500

    sss

--- string_len09.py
+++ string_len09.py
@@ -9,4 +9,9 @@ def main(num1, num2):
     Returns:
         str: answer
     """
-    return
+    return str(int(num1)+int(num2))
+
+
+num1=int(input("sonni kiriitng "))
+num2=int(input("sonni kiriting2"))
+print(main(num1,num2))
